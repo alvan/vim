947d898670fccc2d67d8f3b7891a18da4154edff
Alvan <alvan.law@gmail.com>
2014-08-21 22:51:15 +0800

    delete setup/go.sh

--- setup/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# export GOPATH=~/go
-go get code.google.com/p/go.tools/cmd/goimports
-go get code.google.com/p/rog-go/exp/cmd/godef
-go get github.com/nsf/gocode
-go get github.com/jstemmer/gotags
