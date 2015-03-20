312bfbd48309cddefe9d7a905c68c67152d3c8fd
Alvan <alvan.law@gmail.com>
2015-03-20 16:28:51 +0800

    add solarized mac terminal setup files

---
+++ setup/mac-terminal/solarized-mac-terminal.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# echo "set -o vi" >> ~/.bashrc
+# echo "export TERM=xterm-256color" >> ~/.bash_profile
+echo "export CLICOLOR=1" >> ~/.bash_profile
+echo "export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD" >> ~/.bash_profile
+
