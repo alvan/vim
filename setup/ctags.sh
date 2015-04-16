fa8f2e90f771e753ee8f94ff85ebfc9a10b5a25f
Alvan <alvan.law@gmail.com>
2015-04-16 23:34:54 +0800

    add check if command brew is exists

--- setup/ctags.sh
+++ setup/ctags.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-# sudo apt-get install ctags
-sudo apt-get install exuberant-ctags
+if hash brew 2>/dev/null; then
+    brew install ctags
+else
+    # sudo apt-get install ctags
+    sudo apt-get install exuberant-ctags
+fi
