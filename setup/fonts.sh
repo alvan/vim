f49e7035950c7c449464a2462ef540169ae4e022
Alvan <alvan.law@gmail.com>
2017-07-10 11:49:22 +0800

    update fonts.sh

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 cd "$(dirname "$0")"
 
-cp fonts/* ~/.fonts
-fc-cache -vf ~/.fonts
+if [[ `uname` == 'Darwin' ]]; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+else
+  # Linux
+  font_dir="~/.fonts"
+  mkdir -p $font_dir
+fi
+
+# Copy all fonts to user fonts directory
+echo "Copying fonts..."
+cp fonts/* "$font_dir/"
+
+# Reset font cache on Linux
+if command -v fc-cache @>/dev/null ; then
+    echo "Resetting font cache, this may take a moment..."
+    fc-cache -vf $font_dir
+fi
+
+echo "Powerline fonts installed to $font_dir"
