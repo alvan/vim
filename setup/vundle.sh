2d455085d06212207d895d25891328cb8a4ddfe9
Alvan <alvan.law@gmail.com>
2022-08-27 01:03:27 +0800

    remove vundle setup script

--- setup/vundle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-TOP="$(cd "$(dirname "$0")/../"; pwd)"
-
-# Vundle Plugin
-if [ ! -d "${TOP}/bundle/Vundle.vim" ]; then
-    git clone https://github.com/gmarik/Vundle.vim.git ${TOP}/bundle/Vundle.vim
-fi
-vim -c PluginInstall
